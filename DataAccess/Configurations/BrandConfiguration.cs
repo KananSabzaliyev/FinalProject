c4a12495c539cc51550ce714faf527289bcb694b
KananSabzaliyev <kenansabzaliyev@gmail.com>
2024-05-11 17:49:47 +0400

    Entity and Configuration writed

---
+++ DataAccess/Configurations/BrandConfiguration.cs
@@ -0,0 +1,19 @@
+﻿using Core.DefaultValues;
+using Entities.Concrete.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace DataAccess.Configurations
+{
+    public class BrandConfiguration : IEntityTypeConfiguration<Brand>
+    {
+        public void Configure(EntityTypeBuilder<Brand> builder)
+        {
+            builder.ToTable("Brands");
+            builder.Property(x => x.Id)
+                .UseIdentityColumn(seed: DefaultConstantValue.DEFAULT_PRIMARY_KEY_INCREMENT_VALUE, increment: 1);
+            builder.Property(x => x.Name)
+                .HasMaxLength(50);
+        }
+    }
+}
---
+++ DataAccess/Configurations/GearConfiguration.cs
@@ -0,0 +1,24 @@
+﻿using Core.DefaultValues;
+using Entities.Concrete.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess.Configurations
+{
+    public class GearConfiguration : IEntityTypeConfiguration<Gear>
+    {
+        public void Configure(EntityTypeBuilder<Gear> builder)
+        {
+            builder.ToTable("Gears");
+            builder.Property(x => x.Id)
+                .UseIdentityColumn(seed: DefaultConstantValue.DEFAULT_PRIMARY_KEY_INCREMENT_VALUE, increment: 1);
+            builder.Property(x => x.Name)
+                .HasMaxLength(50);
+        }
+    }
+}
